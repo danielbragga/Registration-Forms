a34976393b0e6c22bb8f2e1569438d997e178bc7
Daniel Braga <danielbraga2121@gmail.com>
2022-07-28 15:47:37 -0300

    add-logic-js

--- index.js
+++ index.js
@@ -2,4 +2,44 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  const firstName = form["firstname"].value;
+  const lastName = form["lastname"].value;
+  const email = form["email"].value;
+  const password = form["password"].value;
+
+  if (firstName === "") {
+    addErrorTo("firstname", "First Name is required");
+  }
+
+  if (lastName === "") {
+    addErrorTo("lastname", "Last Name is required");
+  }
+  if (email === "") {
+    addErrorTo("email", "Email is required");
+  }
+
+  if (isValid(email)) {
+    addErrorTo("email", "Email is not valid");
+  }
+
+  if (password === "") {
+    addErrorTo("password", "Password is required");
+  }
+
+  alert(first + last + email + password);
 });
+
+function addErrorTo(field, message) {
+  const small = form[field].parentNode.querySelector("small");
+  small.innerText = message;
+  small.style.opacity = "1";
+}
+
+const isValid = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
